0a3bcedad4b58c375573d3008e7709a40c46fe51
kaidolive <95751078+kaidolive@users.noreply.github.com>
2021-12-08 19:35:09 +0700

    test
    test

---
+++ ConsoleApp4/ConsoleApp4/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Character
+    {
+        public int Health { get; private set; } = 100;
+        public void Hit(int damage)
+        {
+            if (damage > Health)
+                damage = Health;
+            Health -= damage;
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Character c = new Character();
+            c.Hit(120);
+            Console.WriteLine(c.Health);
+        }
+    }
+}
